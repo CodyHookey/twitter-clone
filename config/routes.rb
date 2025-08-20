b883513850f356fa7a3fcf40906d5560fa8ef61c
Cody Hookey <149050408+CodyHookey@users.noreply.github.com>
2025-08-20 21:04:47 +0100

    Inline with base for kit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   root "static_pages#index"
+
+  namespace :api do
+    # USERS
+    post '/users' => 'users#create'
+
+    # SESSIONS
+    post '/sessions' => 'sessions#create'
+    get '/authenticated' => 'sessions#authenticated'
+    delete '/sessions' => 'sessions#destroy'
+
+    # TWEETS
+    post '/tweets' => 'tweets#create'
+    get '/tweets' => 'tweets#index'
+    delete '/tweets/:id' => 'tweets#destroy'
+    get '/users/:username/tweets' => 'tweets#index_by_user'
+    get '/tweets/search/:keyword' => 'tweets#search'
+  end
+
+  get '*path' => 'static_pages#index'
+  # If you are using active storage to upload and store images, comment the above line
 end
